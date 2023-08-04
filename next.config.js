4cd42fdbd4065a36fc20a3bac310aa01c53596c6
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2023-08-04 10:30:51 +0530

    Aded Next auth setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        'NEXT_AUTH_GOOGLE_SECRET':'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',
+        'AUTH_GOOGLE_CLIENTID':'848282207648-pbu7o6l8m818ms5drbur87m8rpmjbbi0.apps.googleusercontent.com',
+        'NEXTAUTH_SECRET': "tasksphere"
+    }
+}
 
 module.exports = nextConfig
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,11 @@
-export default function Login() {
-  return <div>This is Login</div>;
+
+import { getCurrentUser } from "@/lib/session";
+import SignInbtn from "./SignInbtn";
+
+export default async function Login() {
+  const user = await getCurrentUser()
+  console.log("page" , user);
+  return <div>
+      <SignInbtn user={user}/>
+  </div>;
 }
