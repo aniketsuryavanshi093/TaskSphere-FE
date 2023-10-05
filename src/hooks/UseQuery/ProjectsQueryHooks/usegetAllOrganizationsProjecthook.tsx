afa7a14520ed88a674e884bdf233389186ec497e
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2023-10-06 00:55:51 +0530

    Added pagination and analytics api

--- src/hooks/UseQuery/ProjectsQueryHooks/usegetAllOrganizationsProjecthook.tsx
+++ src/hooks/UseQuery/ProjectsQueryHooks/usegetAllOrganizationsProjecthook.tsx
@@ -1,14 +1,14 @@
 import { getAllOrganizationsProject } from '@/apiServices/admin/adminservices'
 import { useQuery } from '@tanstack/react-query'
 
-const useGetAllOrganizationsProjecthook = (data) => {
+const useGetAllOrganizationsProjecthook = (data, pagination, isanalytic) => {
     return useQuery({
-        queryFn: () => getAllOrganizationsProject(data?.user),
-        queryKey: ['orgainzationprojects', data?.user?.id],
+        queryFn: () => getAllOrganizationsProject({ ...data?.user, ...pagination, isanalytic }),
+        queryKey: ['orgainzationprojects', isanalytic ? `${data?.user?.id}?isanalytic?page=${pagination.currentPage}?itemcount=${pagination.PerpageItemCount}` : data?.user?.id],
         enabled: data?.user?.id && data?.user?.role === "organization" ? true : false,
         refetchOnWindowFocus: false,
         refetchOnReconnect: false,
-        staleTime: 1000 *60 * 5,
+        staleTime: 1000 * 60 * 5,
         retry: false,
         refetchOnmount: false,
     })
